759806923ac59a9db1413093efbc24dc1ea41a00
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-05-19 15:22:44 +0200

    Ruta imágenes app.js

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 app.use('/apiv1/anuncios', require('./routes/apiv1/anuncios'));
 
+app.use('/images/anuncios',
+  express.static(path.join(__dirname, 'public/images')));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
